1970bc8b4482f42a8d098b56165ade208b387ade
Lokesh <lokesh.k1161@gmail.com>
2020-10-21 16:37:36 +0530

    'IPL-Dashboard'

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/styles/variables.scss";`
+      }
+    }
+	},
+	publicPath: process.env.NODE_ENV === 'production'
+		? '/IPL-Dashboard/'
+		: '/',
+	
+};
